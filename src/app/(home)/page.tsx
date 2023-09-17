d413a67172d18dad94890a6f399de97859480d2c
Jevon Cochran <jevon@securebags.com>
2023-09-17 17:52:00 -0500

    latest commit

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import CategoryList from "@/components/home/CategoryList";
@@ -8,6 +8,7 @@ import RangeSelect from "@/components/home/RangeSelect";
 import RatingSelect from "@/components/home/RatingSelect";
 import GoogleMapView from "@/components/home/GoogleMapView";
 import * as globalApi from "@/shared/globalApi";
+import { UserLocationContext } from "@/context/UserLocationContext";
 
 export default function Home() {
   const { data: session } = useSession();
@@ -16,6 +17,9 @@ export default function Home() {
 
   const [category, setCategory] = useState<string>("");
   const [radius, setRadius] = useState<number>(2500);
+  const [businesses, setBusinesses] = useState([]);
+
+  const { userLocation, setUserLocation } = useContext(UserLocationContext);
 
   const onCategoryChange = (category: string) => {
     setCategory(category);
@@ -26,9 +30,12 @@ export default function Home() {
   };
 
   const getGooglePlace = () => {
-    globalApi.getGooglePlace(category, radius).then((res) => {
-      console.log(res.data.product.results);
-    });
+    globalApi
+      .getGooglePlace(category, radius, userLocation.lat, userLocation.lng)
+      .then((res) => {
+        console.log(res.data.product.results);
+        setBusinesses(res.data.product.results);
+      });
   };
 
   useEffect(() => {
--- src/shared/globalApi.ts
+++ src/shared/globalApi.ts
@@ -1,10 +1,17 @@
 import axios from "axios";
 
+// TODO: Fix this!!!
 // For some reason, the search results return places near user current location when radius is changed
 // This even though I have the lat and lng hard-coded to Oakland, CA at the moment
-const getGooglePlace = (category: string, radius: number) => {
+// This is only happening when raidus is undefined or 0 I believe
+const getGooglePlace = (
+  category: string,
+  radius: number,
+  lat: number,
+  lng: number
+) => {
   return axios.get(
-    `/api/google-place?category=${category}&radius=${radius}&lat=37.804363&lng=-122.271111`
+    `/api/google-place?category=${category}&radius=${radius}&lat=${lat}&lng=-${lng}`
   );
 };
 
